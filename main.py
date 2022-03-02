86157154c92ab0626bb7d1135cc4c5ccd121ae39
manuele8 <75259748+manuele8@users.noreply.github.com>
2022-03-02 16:48:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ class pythonOrgSearch(unittest.TestCase):
     print("setup")
     self.driver = webdriver.Chrome(path)
     self.driver.get(url)
-   
-  def test_title(self):
-    mainPage = page.MainPage()
+  
+  def test_search_python(self):
+    mainPage = page.MainPage(self.driver)
     assert mainPage.is_title_matches()
+    mainPage.search_text_element = "pycon"
+    mainPage.click_go_button()
+    search_result_page = page.SearchResultPage(self.driver)
+    assert search_result_page.is_results_found()
   
   def tearDown(self):
     self.driver.close()
