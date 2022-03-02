6ee16fd665abfcc24f418b8e745210acc629294a
manuele8 <75259748+manuele8@users.noreply.github.com>
2022-03-02 14:55:00 +0100

    Update Cheat_Youtube_Iscritti1!1!!1!.py

--- Cheat_Youtube_Iscritti1!1!!1!.py
+++ Cheat_Youtube_Iscritti1!1!!1!.py
@@ -1,6 +1,22 @@
+import unittest
 from selenium import webdriver
-PATH = r'C:\Users\Manu\Desktop\Tutto\App\chromedriver.exe'
+import page 
+path = r'C:\Users\Manu\Desktop\Tutto\App\chromedriver.exe'
 url = 'https://www.youtube.com'
-driver = webdriver.Chrome(PATH)
 
-driver.get(url)
+class pythonOrgSearch(unittest.TestCase):
+  
+  def setUp(self):
+    print("setup")
+    self.driver = webdriver.Chrome(path)
+    self.driver.get(url)
+   
+  def test_title(self):
+    mainPage = page.MainPage()
+    assert mainPage.is_title_matches()
+  
+  def tearDown(self):
+    self.driver.close()
+    
+if __name__ == "__main__":
+  unittest.main()
